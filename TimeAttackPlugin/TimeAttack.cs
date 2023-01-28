f337825e1dccb09072d8760f14f3437050312db7
gTapir <gamingtapir@gmail.com>
2023-01-28 23:39:26 +0100

    Initial commit.

---
+++ TimeAttackPlugin/TimeAttack.cs
@@ -0,0 +1,37 @@
+﻿using AssettoServer.Network.Packets;
+using AssettoServer.Network.Packets.Incoming;
+using AssettoServer.Network.Packets.Outgoing;
+using AssettoServer.Server.GeoParams;
+using AssettoServer.Server;
+using AssettoServer.Server.Plugin;
+using AssettoServer.Network.Tcp;
+using Serilog;
+
+namespace TimeAttackPlugin;
+
+public class TimeAttack : IAssettoServerAutostart
+{
+    public TimeAttack(CSPClientMessageTypeManager clientMessageTypeManager)
+    {
+        Log.Debug("Initializing TimeAttackPlugin");
+        clientMessageTypeManager.RegisterClientMessageType(0x91C1F6D7, IncomingTimeAttackResult);
+        Log.Debug("Successfully registered ClientMessageType for TimeAttack");
+    }
+
+    private static void IncomingTimeAttackResult(ACTcpClient client, PacketReader reader)
+    {
+        Log.Debug($"Recieved a message from {client.Name}. Trying to read it now...");
+        string value = reader.ReadUTF32String();
+        Log.Debug($"Read content of TimeAttack message. It contained the following: {value}");
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ TimeAttackPlugin/TimeAttackModule.cs
@@ -0,0 +1,12 @@
+﻿using AssettoServer.Server.Plugin;
+using Autofac;
+
+namespace TimeAttackPlugin;
+
+public class TimeAttackModule : AssettoServerModule
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<TimeAttack>().AsSelf().AutoActivate().SingleInstance();
+    }
+}
